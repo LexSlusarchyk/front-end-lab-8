e2ec6b2d0107ba531b4c150ab6513817a728d0bd
Lex_Slusarchyk <lexslusarchyk@gmail.com>
2018-02-23 14:27:39 +0200

    start homework 10 done task 1

---
+++ homework_10/task1.js
@@ -0,0 +1,35 @@
+function debounce(f, ms) {
+
+	let timeout = null;
+	return () => {
+		if (timeout) {
+		  clearTimeout(timeout);
+		}
+		timeout = setTimeout(f, ms);
+	};
+}
+
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 2'
