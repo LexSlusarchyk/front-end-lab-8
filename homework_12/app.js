a3288b8aed05714637b09f6fb155e2ae45bf37e3
Lex_Slusarchyk <lexslusarchyk@gmail.com>
2018-03-01 00:02:58 +0200

    to be continued homework 12

--- homework_12/app.js
+++ homework_12/app.js
@@ -1,46 +1,96 @@
 'use strict';
-let rootNode = document.getElementById("root");
+const rootNode = document.getElementById("root");
+const tankPreview = document.createElement("div");
 
-let headline = document.createElement("h1");
+const headline = document.createElement("h1");
 headline.innerHTML = "Most popular tanks";
+tankPreview.appendChild(headline);
 
-rootNode.appendChild(headline);
+const tanksColection = document.createElement("div");
+tanksColection.classList = "tanksColection";
 
 tanks.forEach((item) => tanksList(item));
 
-function tanksList(item){
-  let tank = document.createElement("div");
-  tank.classList = "tank";
-  let tankDesc = document.createElement("div");
-  tankDesc.classList = "tank-desc";
+tankPreview.appendChild(tanksColection);
+rootNode.appendChild(tankPreview);        
 
-  let tankPreview = document.createElement("img");
+function tanksList(item){
+  const tank = document.createElement("div");
+  tank.classList = "tank";
+  tank.addEventListener('click', () => location.hash = item.model); //Change hash
+
+  const tankPreview = document.createElement("img");
   tankPreview.setAttribute('src', item.preview);
   tank.appendChild(tankPreview);
 
-  let flag = document.createElement("img");
+  const tankDesc = document.createElement("div");
+  tankDesc.classList = "tank-desc";
+
+  const flag = document.createElement("img");
   flag.setAttribute('src', item.country_image);
   tankDesc.appendChild(flag);
 
-  let tankLvl = document.createElement("p");
-  tankLvl.innerHTML = item.level;
-  tankDesc.appendChild(tankLvl);
+  const mainInfo = document.createElement("h4");
+  mainInfo.innerHTML = `${item.level}  ${item.model.toUpperCase()}`;
+  tankDesc.appendChild(mainInfo);
   
-  let tankModel = document.createElement("p");
-  tankModel.innerHTML = item.model.toUpperCase();
-  tankDesc.appendChild(tankModel);
-
-  
-
-  
-
-  
-
   tank.appendChild(tankDesc);
-  rootNode.appendChild(tank);
-
-   console.log();
+  tanksColection.appendChild(tank);
 }
 
+const tankDetails = document.createElement("div");
+function detailsView(){
+
+  const model = location.hash.slice(1);
+  const tank = tanks.find((item) => item.model === model ? item : null);
+
+  const description = document.createElement("div");
+
+  const flag = document.createElement("img");
+  flag.setAttribute('src', tank.country_image);
+  description.appendChild(flag);
+  const mainInfo = document.createElement("h4");
+  mainInfo.innerHTML = `${tank.model.toUpperCase()} (level ${tank.level})`;
+  description.appendChild(mainInfo);
+
+  tankDetails.appendChild(description);
+
+  const preview = document.createElement("h4");
+  preview.innerHTML = "Preview";
+  tankDetails.appendChild(preview);
+
+  const tankImg = document.createElement("img");
+  tankImg.setAttribute('src', tank.preview);
+  tankDetails.appendChild(tankImg);
 
 
+  //Table Characteristic
+  const table = document.createElement("table");
+  const caption = document.createElement("caption");
+  caption.innerHTML = "Characteristic";
+  table.appendChild(caption);
+  for (let prop in tank.details){
+    const tr = document.createElement("tr");
+    table.appendChild(tr);
+    const tdProp = document.createElement("td");
+    tdProp.innerHTML = prop;
+    tr.appendChild(tdProp);
+    const tdValue = document.createElement("td");
+    tdValue.innerHTML = tank.details[prop];
+    tr.appendChild(tdValue);
+  }
+  tankDetails.appendChild(table);
+  
+  const back = document.createElement("a");
+  back.innerHTML = "Back to list view";
+  back.setAttribute("href", "");
+
+  tankDetails.appendChild(back);
+
+  rootNode.replaceChild(tankDetails, tankPreview);   
+}
+
+//differentiate which content to render 
+window.addEventListener('hashchange',
+  () => {location.hash ? detailsView() : rootNode.replaceChild(tankPreview, tankDetails)}
+);
