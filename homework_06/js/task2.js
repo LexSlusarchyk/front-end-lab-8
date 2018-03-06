93177c8d9d263a83a7a9a9bac3d37e379a3e1b35
Lex_Slusarchyk <lexslusarchyk@gmail.com>
2018-03-06 20:32:36 +0200

    fix homework 6

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -8,17 +8,16 @@ const eur = 33.23;
 let eurValue = parseFloat(prompt("Enter the amount of currency (Euro): "));
 let usdValue = parseFloat(prompt("Enter the amount of currency (Dollar): "));
 
-
 if (eurValue && usdValue && eurValue > 0 && usdValue > 0) { //check if currency format is correct
-	//Convert EUR to UAH
-	let eurToUah = eur * eurValue;
-	//Convert USD to EUR
-	let usdToUah = usd * usdValue;
-	//Convert EUR to USD
-	let eurToUsd = eur / usd;
-	// Return result to console
-	console.log(eurValue + " euros are equal " + eurToUah.toFixed(2) + " UAH, " + usdValue +
+//Convert EUR to UAH
+  let eurToUah = eur * eurValue;
+//Convert USD to EUR
+  let usdToUah = usd * usdValue;
+//Convert EUR to USD
+  let eurToUsd = eur / usd;
+// Return result to console
+  console.log(eurValue + " euros are equal " + eurToUah.toFixed(2) + " UAH, " + usdValue + 
   	" dollars are equal " + usdToUah.toFixed(2) + " UAH, one euro is equal " + eurToUsd.toFixed(2) + " dollars.");
 } else {
-	console.log("Incorrect data"); //Throw error
+  console.log("Incorrect data"); //Throw error
 }
